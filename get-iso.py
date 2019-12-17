6717720c43946ef24cdef053f9c6a1acae36477f
nnsense <nnsense@hotmail.it>
2019-12-17 02:29:41 +0000

    - Added other CentOS versions (6 and 8) - Removed some unnecessary code - Added a timeout to SpeedTest (max 2 secs) - Default action is now listing all URL (previously --verbose) along the best speed URL - -v is now CentOS version (6, 7 or 8) - Since --download is now required to download, confirmation is unnecessary (removed) - Deleting the python3 non-thread version. Temporarily leaving python2 version

--- get-iso.py
+++ get-iso.py
@@ -1,29 +1,31 @@
 # pip install requests urllib3 pyOpenSSL bs4 --force --upgrade
 import sys
+import re
 import urllib
 import requests
 import argparse
 from threading import Thread
 from asyncio import Queue
-from pprint import pprint
-# import logging
 from bs4 import BeautifulSoup
 
-parser = argparse.ArgumentParser( description="Search and download the latest CentOS7 ISO selecting the best speed mirror.", epilog="By nnsense - 2018")
-parser.add_argument("-v","--verbose", help="Show each mirror's speed and ask if download latest image (by default, it just select and downloads the ISO)", required=False, action="store_true")
-parser.add_argument("-l","--list", help="Show best speed download link and exit", required=False, action="store_true")
+parser = argparse.ArgumentParser( description="Search and optionally download the latest CentOS7 ISO selecting the best speed mirror.", epilog="By nnsense - 2018")
+parser.add_argument("-v","--version", help="Specify CentOS version: 6, 7 or 8. Default: 7", required=False, default=7)
+parser.add_argument("-d","--download", help="Download the ISO", required=False, action="store_true")
 parser.add_argument("-t","--tree", help="Show best speed directory tree (for kickstart) and exit", required=False, action="store_true")
 args = parser.parse_args()
-
-# logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-10s) %(message)s', )
 
 
 def main():
 
-    url = "http://isoredirect.centos.org/centos/7/isos/x86_64"
+    centos_version = str(args.version)
+
+    url = "http://isoredirect.centos.org/centos/" + centos_version + "/isos/x86_64"
+
     f = urllib.request.urlopen(url)
     html = f.read()
-    source = BeautifulSoup(html, 'html.parser');
+    source = BeautifulSoup(html, 'html.parser')
+    
+    
     
     threads = []
     queue = Queue()
@@ -33,7 +35,6 @@ def main():
         href = str(link.string)
         
         if href.startswith('http') and 'x86_64' in href:
-        
             thread = Thread( target=TestSpeed, args=( href,queue ) )
             thread.start()
             threads.append(thread)
@@ -41,56 +42,43 @@ def main():
     for th in threads:
         th.join()
 
-    bestspeed = 0.2
+    bestspeed = 0.5
     
     while not queue.empty():
-        speedtest = queue.get_nowait()
+        speedtst = queue.get_nowait()
 
-        besturl = speedtest['url']
-        speed = speedtest['speed']
+        besturl = speedtst['url']
+        speed = speedtst['speed']
                 
-        if args.verbose:
-            print(str(speed) + " - Mirror: " + url)
+        print(str(speed) + " - Mirror: " + url)
             
         if speed < bestspeed:
             bestspeed = speed
             besturl = url
 
 
-    print( "Best speed: " + besturl + " (speed: " + str(bestspeed) + ")" )
-
-    # If verbose enable interactive
-    isoAsk = "n"
-
-    if args.verbose and not args.list:
-        isoAsk = raw_input('Download latest CentOS7? (Y/n): ')
-
-        while isoAsk != "Y" and isoAsk != "n":
-            isoAsk = raw_input('Download latest CentOS7? (Y/n): ')
-
-    # As before, fetch page
-    f = urllib.request.urlopen(besturl)
-    
-    html_linksPage = f.read()
-    linksPage = BeautifulSoup(html_linksPage, 'html.parser');
-
-    # For each link
-    for link in linksPage.find_all('a'):
-        href = str(link.string)
-        if ( 'torrent' not in href and 'Minimal' in href ):
-            file_name = href.rstrip()
-    if args.list and not args.tree:
-        print("Best download option: " + besturl + "/" + file_name)
-        exit()
+    print( "-- Best speed: " + besturl + " (speed: " + str(bestspeed) + ")" )
 
     if args.tree:
         dirtree = besturl.replace("isos","os")
         print("Use directory tree: " + dirtree)
-        exit()
 
-    # Downloading the ISO
-    if isoAsk == "Y" or args.verbose == False:
+    if args.download:
+        # Fetch the new page from the best speed link
+        f = urllib.request.urlopen(besturl)
+        
+        html_linksPage = f.read()
+        linksPage = BeautifulSoup(html_linksPage, 'html.parser');
+
+        # For each link
+        for link in linksPage.find_all('a'):
+            href = str(link.string)
+
+            if(re.search('minimal.*iso$', href.lower(), re.IGNORECASE) or re.search('boot.*iso$', href.lower(), re.IGNORECASE)):
+                    file_name = href.rstrip()
+
         with open(file_name, "wb") as f:
+
             print("Downloading %s" % file_name)
             
             response = requests.get(besturl + "/" + file_name, stream=True)
@@ -107,15 +95,15 @@ def main():
                     done = int(50 * dl / total_length)
                     sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )    
                     sys.stdout.flush()
-                    print()
-    else:
-        print("Quitting")
 
 
 def TestSpeed(href, queue):
-    speed = requests.get(href.rstrip()).elapsed.total_seconds()
-    url = href.rstrip()
-    speedtest = {"url": url, "speed": speed}
-    queue.put_nowait(speedtest)
-
+    try:
+        speed = requests.get(href.rstrip(), timeout=1).elapsed.total_seconds()
+        url = href.rstrip()
+        speedtest = {"url": url, "speed": speed}
+        queue.put_nowait(speedtest)
+    except:
+        pass
+        
 if __name__ == '__main__': main()
